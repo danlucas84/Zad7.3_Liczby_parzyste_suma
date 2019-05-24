8a18add2297fb6e140aeb78029cb20797ebc3157
Dell <dandan_21@o2.pl>
2019-05-24 15:07:15 +0200

    poprawiono formatowanie

--- src/LiczbyParzyste.java
+++ src/LiczbyParzyste.java
@@ -1,24 +1,19 @@
-public class LiczbyParzyste
-{
+public class LiczbyParzyste {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int i;
         int sum = 0;
 
 
         System.out.println("Program wyświetla liczby parzyste od 0 do 100");
 
-        for (i = 0;  i< 101; i++)
+        for (i = 0; i < 101; i++) {
 
+            if (i < 101 && i % 2 == 0)
+                System.out.println(i + " ");
 
-        {
-
-            if ( i< 101 && i%2==0)
-                System.out.println(i +" ");
-
-            if (i%2==0)
-            sum = sum + i;
+            if (i % 2 == 0)
+                sum = sum + i;
         }
         System.out.println("Wyswietlono sumę liczb parzystych od 0 do 100 - suma wynosi:" + sum);
     }
